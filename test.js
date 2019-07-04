976886c019fb3f526d3ee22351f732bb7e8b74d0
Raees Iqbal <raees.bhatti@gmail.com>
2019-07-04 23:36:41 +0500

    Revert "Update a test check"
    This reverts commit b8f804536899862846fc596ab45396b573cdb07e.

--- test.js
+++ test.js
@@ -89,7 +89,7 @@ test("Wrong base array structure", t => {
   }, Error);
   t.is(
     error.message,
-    'Schema is looking for "array", data is {"power1":"fire","power2":"ice"}'
+    'Schema is looking for "array", data is {"power1":"fire","power2":"magic"}'
   );
 });
 
