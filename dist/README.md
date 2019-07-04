c66571c8e6576fc2449344ff49db39e3b070d9e9
Jacob Schatz <jschatz1@gmail.com>
2019-07-04 07:14:09 -0400

    proper bundling of files for webpack/browser/etc

---
+++ dist/README.md
@@ -0,0 +1,9 @@
+## Explanation of Build Files
+
+### Terms
+
+- **[UMD](https://github.com/umdjs/umd)**: UMD builds can be used directly in the browser via a `<script>` tag.
+
+- **[CommonJS](http://wiki.commonjs.org/wiki/Modules/1.1)**: CommonJS builds are intended for use with older bundlers like [browserify](http://browserify.org/) or [webpack 1](https://webpack.github.io).
+
+- **[ES Module](http://exploringjs.com/es6/ch_modules.html)**: ES module builds are intended for use with modern bundlers like [webpack 2](https://webpack.js.org) or [rollup](http://rollupjs.org/).
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { uglify } from "rollup-plugin-uglify";
 
 export default [{
   input: 'src/index.js',
@@ -16,9 +17,35 @@ export default [{
 },{
   input: 'src/index.js',
   output: {
+    file: 'dist/mcgorgeous.js',
+    format: 'umd',
+    name: 'mcgorgeous'
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+},{
+  input: 'src/index.js',
+  output: {
     file: 'dist/mcgorgeous.min.js',
     format: 'umd',
     name: 'mcgorgeous'
+  },
+  plugins: [
+    resolve(),
+    uglify(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+},{
+  input: 'src/index.js',
+  output: {
+    file: 'dist/mcgorgeous.esm.js',
+    format: 'esm'
   },
   plugins: [
     resolve(),
